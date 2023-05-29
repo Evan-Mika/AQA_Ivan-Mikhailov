70053f056e2f4836cbdc400cd48fb262a7aa0a29
Evan-Mika <124917163+Evan-Mika@users.noreply.github.com>
2023-05-29 12:01:30 +0200

    lesson_3

---
+++ home_work_3/src/main/java/main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 11;
+
+        int sum = a + b;
+        System.out.println(sum);
+
+        int diff = a - b;
+        System.out.println(diff);
+
+        int mult = a * b;
+        System.out.println(mult);
+
+        float dev = (float) a / b;
+        System.out.println(dev);
+    }
+}
