2f591a87df8483b5f955ed86bea7e7c6fce4f05c
Evan-Mika <124917163+Evan-Mika@users.noreply.github.com>
2023-06-09 19:55:51 +0200

    Lesson_4 + (file Main) + added old tasks number : 1,2,6,7,8

--- home_work_4_retake_2/src/main/java/Main.java
+++ home_work_4_retake_2/src/main/java/Main.java
@@ -4,7 +4,24 @@ public class Main {
 
 
      public static void main(String [] args) {
-            // task 3
+
+
+         // task 1
+         double d = 2.54;
+         d = 10 / d;
+         System.out.println(d);
+         double d1 = 2.54;
+         d1 = d1 * 25;
+         System.out.println(d1);
+
+
+         // task 2
+         int euro = 28;
+         euro = 10000 / euro;
+         System.out.println(euro);
+
+
+         // task 3
             Scanner userInput = new Scanner(System.in);
             int x = userInput.nextInt();
 
@@ -27,6 +44,7 @@ public class Main {
 
             }
 
+
             // task 5
 
             Scanner scanner = new Scanner(System.in);
@@ -37,5 +55,44 @@ public class Main {
                 int weight = ed + (x / 10);
                 System.out.println(weight);
             }
-        }
+
+
+         // task 6
+
+         Scanner scanner = new Scanner(System.in);
+         int x = scanner.nextInt();
+
+         if (x > 0) {
+             System.out.println("положительное");
+         }
+         if (x < 0) {
+             System.out.println("отрицательное");
+         }
+         if (x == 0) {
+             System.out.println("ноль"); }
+
+
+         // task 7
+         Scanner scanner = new Scanner(System.in);
+         int x = scanner.nextInt();
+
+         if (x <= 100) {
+             int result = --x;
+             System.out.println(result); }
+
+
+         // task 8
+         Scanner scanner = new Scanner(System.in);
+         int a = scanner.nextInt();
+
+         if (a > 9 && a < 100) {
+             int ed = a % 10;
+             int result = ed * (a / 10);
+             System.out.println(result);
+             if (a > result) {
+                 System.out.println("a больше");
+             } else {
+                 System.out.println("a меньше"); }
+         }
+      }
     }
