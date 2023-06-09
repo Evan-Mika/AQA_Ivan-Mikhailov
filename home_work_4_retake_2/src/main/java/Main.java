8f61f62be5d710ad4d1fc7586ae891694beed41c
Evan-Mika <124917163+Evan-Mika@users.noreply.github.com>
2023-06-09 11:40:50 +0200

    Lesson_4 + (file Main)

--- home_work_4_retake_2/src/main/java/Main.java
+++ home_work_4_retake_2/src/main/java/Main.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Main {
-}
+import java.util.Scanner;
+
+public class Main {
+
+
+     public static void main(String [] args) {
+            // task 3
+            Scanner userInput = new Scanner(System.in);
+            int x = userInput.nextInt();
+
+            if (x > 9 && x < 100) {
+                int ed = x % 10;
+                int des = x / 10;
+                System.out.print(ed + " " + des);
+            }
+
+            // task 4
+
+            Scanner userInput = new Scanner(System.in);
+            int x = userInput.nextInt();
+
+            if (x > 9 && x < 100) {
+
+                int ed = x % 10;
+                int des = x - ed;
+                System.out.print(des + " + " + ed);
+
+            }
+
+            // task 5
+
+            Scanner scanner = new Scanner(System.in);
+            int x = scanner.nextInt();
+
+            if (x > 9 && x < 100) {
+                int ed = x % 10;
+                int weight = ed + (x / 10);
+                System.out.println(weight);
+            }
+        }
+    }
